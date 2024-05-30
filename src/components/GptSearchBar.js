233572311941354ca412f6f8d3d6c65bf4aed13d
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-05-31 00:22:11 +0530

    Multi language support in GPT search page

---
+++ src/components/GptSearchBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import lang from "../utils/languageConstant";
+import { useSelector } from "react-redux";
+
+const GptSearchBar = () => {
+  const selectedLang = useSelector((store) => store.config?.lang);
+  return (
+    <div className="pt-[10%] flex justify-center">
+      <form className="bg-black grid grid-cols-12 w-1/2">
+        <input
+          type="text"
+          className="p-4 m-4 col-span-9"
+          placeholder={lang[selectedLang].gptSearchPlaceholder}
+        />
+        <button className="py-2 px-4 bg-red-500 text-white rounded-lg col-span-3 m-4">
+          {lang[selectedLang].search}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default GptSearchBar;
